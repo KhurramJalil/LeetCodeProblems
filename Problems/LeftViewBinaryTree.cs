5dd1c14e1f7f8a4c3bd20826abc00fbe3eeacd15
Khurram Jalil <khurram.jalil@gmail.com>
2022-10-31 19:29:25 +0500

    Added LeftViewBinaryTree and RightViewBinaryTree problems in the solution

---
+++ Problems/LeftViewBinaryTree.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Problems
+{
+    internal class LeftViewBinaryTree
+    {
+        public static IList<int> SolveIt(TreeNode root)
+        {
+            var visitedLevel = new List<int>();
+            var result = new List<int>();
+
+            leftView(root, 0, visitedLevel, result);
+
+            return result;
+        }
+
+        static void leftView(TreeNode currNode, int level, List<int> visitedLevel, List<int> result)
+        {
+            if (currNode != null)
+            {
+                if (!visitedLevel.Contains(level))
+                {
+                    visitedLevel.Add(level);
+                    result.Add(currNode.val);
+                }
+                level++;
+                leftView(currNode.left, level, visitedLevel, result);
+                leftView(currNode.right, level, visitedLevel, result);
+            }
+        }
+    }
+}
---
+++ Problems/RightViewBinaryTree.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Problems
+{
+    internal class RightViewBinaryTree
+    {
+        public static IList<int> SolveIt(TreeNode root)
+        {
+            var visitedLevel = new List<int>();
+            var result = new List<int>();
+
+            rightView(root, 0, visitedLevel, result);
+
+            return result;
+        }
+
+        static void rightView(TreeNode currNode, int level, List<int> visitedLevel, List<int> result)
+        {
+            if (currNode != null)
+            {
+                if (!visitedLevel.Contains(level))
+                {
+                    visitedLevel.Add(level);
+                    result.Add(currNode.val);
+                }
+                level++;                
+                rightView(currNode.right, level, visitedLevel, result);
+                rightView(currNode.left, level, visitedLevel, result);
+            }
+        }
+    }
+}
