d05188a24181f6846400a89cf22b4f1cddcb13e5
Khurram Jalil <khurram.jalil@gmail.com>
2022-11-01 12:49:38 +0500

    Added alternate solutions for both Left View and Right View of binary tree problems

--- Problems/LeftViewBinaryTree.cs
+++ Problems/LeftViewBinaryTree.cs
@@ -9,6 +9,7 @@ namespace LeetCodeProblems.Problems
 {
     internal class LeftViewBinaryTree
     {
+        #region Solution 1
         public static IList<int> SolveIt(TreeNode root)
         {
             var visitedLevel = new List<int>();
@@ -33,5 +34,31 @@ namespace LeetCodeProblems.Problems
                 leftView(currNode.right, level, visitedLevel, result);
             }
         }
+        #endregion
+
+        #region Solution 2
+        public static IList<int> SolveIt2(TreeNode root)
+        {
+            var result = new Dictionary<int, int>();
+
+            leftView(root, 0, result);
+
+            return result.Values.ToList();
+        }
+
+        static void leftView(TreeNode currNode, int level, Dictionary<int, int> result)
+        {
+            if (currNode != null)
+            {
+                if (!result.ContainsKey(level))
+                {
+                    result[level] = currNode.val;
+                }
+                level++;
+                leftView(currNode.left, level, result);
+                leftView(currNode.right, level, result);                
+            }
+        }
+        #endregion
     }
 }
--- Problems/RightViewBinaryTree.cs
+++ Problems/RightViewBinaryTree.cs
@@ -9,6 +9,7 @@ namespace LeetCodeProblems.Problems
 {
     internal class RightViewBinaryTree
     {
+        #region Solution 1
         public static IList<int> SolveIt(TreeNode root)
         {
             var visitedLevel = new List<int>();
@@ -28,10 +29,36 @@ namespace LeetCodeProblems.Problems
                     visitedLevel.Add(level);
                     result.Add(currNode.val);
                 }
-                level++;                
+                level++;
                 rightView(currNode.right, level, visitedLevel, result);
                 rightView(currNode.left, level, visitedLevel, result);
             }
         }
+        #endregion
+
+        #region Solution 2
+        public static IList<int> SolveIt2(TreeNode root)
+        {
+            var result = new Dictionary<int, int>();
+
+            rightView(root, 0, result);
+
+            return result.Values.ToList();
+        }
+
+        static void rightView(TreeNode currNode, int level, Dictionary<int, int> result)
+        {
+            if (currNode != null)
+            {
+                if (!result.ContainsKey(level))
+                {
+                    result[level] = currNode.val;
+                }
+                level++;
+                rightView(currNode.right, level, result);
+                rightView(currNode.left, level, result);
+            }
+        }
+        #endregion
     }
 }
